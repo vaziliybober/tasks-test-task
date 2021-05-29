16b9482cfa82181aaf8805b55862eda1c160f4c1
vaziliybober <vaziliybober@gmail.com>
2021-05-29 08:53:25 +0300

    add useTenantGUID hook

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import Header from "./Header.js";
 import Sidebar from "./Sidebar.js";
 import Main from "./Main.js";
 import "../css/App.css";
+import useTenantGUID from "../hooks/useTenantGUID.js";
 
 function App() {
   const [currentSection, setCurrentSection] = React.useState("applications");
+  const tenantGUID = useTenantGUID();
+
   return (
     <div className="App">
       <Sidebar
@@ -16,6 +19,7 @@ function App() {
         <Header />
         <Main />
         <div>{currentSection}</div>
+        <div>{tenantGUID || "no"}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import axios from "axios";
+import "./css/index.css";
+import App from "./components/App.js";
+
+const queryClient = new QueryClient();
+
+axios.defaults.baseURL = "http://intravision-task.test01.intravision.ru";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
